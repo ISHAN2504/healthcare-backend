0de6d9ae86c3a438677c6d9ac0739141264b8b0e
ISHAN AGRAWAL <agrawalishan2004@gmail.com>
2025-09-06 10:59:41 +0530

    Added Render DB connection

--- src/db.js
+++ src/db.js
@@ -1,9 +1,11 @@
-// src/db.js
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // from Supabase/Render
+  connectionString: process.env.DATABASE_URL, // Render provides this
+  ssl: {
+    rejectUnauthorized: false, // important for Render PostgreSQL
+  },
 });
 
 export default pool;
